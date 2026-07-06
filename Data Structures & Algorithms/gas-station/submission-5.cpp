c46d3d3baafc2b2afb0813b56579665c77ca71ad
Temirlan <mukhanovtemirlanb@gmail.com>
2026-07-06 16:54:32 +0800

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        vector<int> net;
+        int n = gas.size();
+        vector<int> cumsum(1, 0);
+        for (int i = 0; i < n; ++i) {
+            net.push_back(gas[i] - cost[i]);
+            cumsum.push_back(gas[i] - cost[i] + cumsum.back());
+        }
+
+        if (cumsum.back() < 0) return -1;
+        for (int i = 0; i < n; ++i) {
+            if(net[i] >= 0) {
+                int curr = 0;
+                bool good = true;
+                for (int j = i; j < n; ++j) {
+                    curr += net[j];
+                    if(curr < 0) {good = false; break;}
+                }
+                if (!good) continue;
+                for(int j = 0; j < i; j++) {
+                    curr += net[j];
+                    if(curr < 0) {good = false; break;}
+                }
+                if (good || curr == 0) return i;
+            }
+        }
+        return -1;
+    }
+};
